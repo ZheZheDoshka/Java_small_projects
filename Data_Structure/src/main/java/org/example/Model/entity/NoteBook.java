2b9cdce07a3811defe3c139b82754666ec009121
ZheZheDoshka <grayfox380@gmail.com>
2021-09-07 19:55:32 +0300

    Added most of stuff, implementation left

---
+++ Data_Structure/src/main/java/org/example/Model/entity/NoteBook.java
@@ -0,0 +1,130 @@
+package org.example.Model.entity;
+
+public class NoteBook {
+    private String First_name;
+    private String Surname;
+    private String Nickname;
+    private String FIO;
+    private String Login;
+    private String Group;
+    private String Home_Phone;
+    private String Phone_Number;
+    private String Phone_Number_2;
+    private String Email;
+    private String Skype;
+    private String Address;
+    private String Date;
+    private String Last_Change;
+
+    public String getFirst_name() {
+        return First_name;
+    }
+
+    public void setFirst_name(String First_name) {
+        this.First_name = First_name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public void setFIO(String FIO) {
+        this.FIO = FIO;
+    }
+
+    public String getGroup() {
+        return Group;
+    }
+
+    public void setGroup(String group) {
+        Group = group;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getHome_Phone() {
+        return Home_Phone;
+    }
+
+    public void setHome_Phone(String home_Phone) {
+        Home_Phone = home_Phone;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public String getPatronymic() {
+        return Nickname;
+    }
+
+    public void setPatronymic(String nickname) {
+        Nickname = nickname;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getLast_Change() {
+        return Last_Change;
+    }
+
+    public void setLast_Change(String last_Change) {
+        Last_Change = last_Change;
+    }
+
+    public String getPhone_Number() {
+        return Phone_Number;
+    }
+
+    public void setPhone_Number(String phone_Number) {
+        Phone_Number = phone_Number;
+    }
+
+    public String getPhone_Number_2() {
+        return Phone_Number_2;
+    }
+
+    public void setPhone_Number_2(String phone_Number) {
+        Phone_Number_2 = phone_Number;
+    }
+
+    public String getSkype() {
+        return Skype;
+    }
+
+    public void setSkype(String skype) {
+        Skype = skype;
+    }
+}
--- Data_Structure/src/main/java/org/example/View/View.java
+++ Data_Structure/src/main/java/org/example/View/View.java
@@ -4,12 +4,12 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class View {
-    static String MESSAGES_BUNDLE_NAME = "messages"; //messages resource bundle WIP
+    static String MESSAGES_BUNDLE_NAME = "messages"; //messages_en resource bundle WIP
     public static final ResourceBundle bd =
             ResourceBundle.getBundle(
                     MESSAGES_BUNDLE_NAME,
-                    new Locale("ua", "UA"));
-                    //new Locale("en"));           //for english
+                    //new Locale("ua", "UA"));              //for Ukrainian
+                    new Locale("en"));           //for english
 
     public void printMessage(String message){
         System.out.println(message);

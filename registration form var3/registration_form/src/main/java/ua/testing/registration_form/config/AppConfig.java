077d22cb742dd63b2f458bd26e8e1939c58e4704
ZheZheDoshka <grayfox380@gmail.com>
2021-10-04 11:41:23 +0300

    Spring. Understood. Localization is almost done

--- registration form var3/registration_form/src/main/java/ua/testing/registration_form/config/AppConfig.java
+++ registration form var3/registration_form/src/main/java/ua/testing/registration_form/config/AppConfig.java
@@ -3,12 +3,15 @@ package ua.testing.registration_form.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.Locale;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
         CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
@@ -18,4 +21,13 @@ public class AppConfig {
         return cookieLocaleResolver;
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
 }
